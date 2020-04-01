f4fe22d0a49bda22529bca9da0b4bcbd12cdebc2
adleywd <adleydamaceno@gmail.com>
2020-03-31 22:01:57 -0300

    Handling errors

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged in successfuly');
     }, error => {
-      console.error('Fail to login');
+      console.error(error);
     });
   }
 
