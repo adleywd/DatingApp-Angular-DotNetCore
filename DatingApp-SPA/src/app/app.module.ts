3967a308e2db043189c83e058314f9563d6ce35b
adleywd <adleydamaceno@gmail.com>
2020-04-15 01:32:37 -0300

    Add user edit profile interface

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -27,6 +27,9 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './resolvers/member-detail.resolver';
 import { MemberListResolver } from './resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './guards/prevent-unsaved-changes.guard';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -43,7 +46,8 @@ export function tokenGetter() {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
-      MemberDetailComponent
+      MemberDetailComponent,
+      MemberEditComponent
    ],
    imports: [
       BrowserModule,
@@ -67,7 +71,9 @@ export function tokenGetter() {
       UserService,
       ErrorInterceptorProvider,
       MemberDetailResolver,
-      MemberListResolver
+      MemberListResolver,
+      MemberEditResolver,
+      PreventUnsavedChanges
    ],
    bootstrap: [
       AppComponent
---
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/models/User';
+import { AlertifyService } from 'src/app/services/alertify.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm', { static: true }) editForm: NgForm;
+  user: User;
+
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = false;
+    }
+  }
+
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data[`user`];
+      console.log(this.user);
+    });
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile Update successfuly.');
+    this.editForm.reset(this.user);
+  }
+}
