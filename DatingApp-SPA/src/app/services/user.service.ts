60f75608dd8c238bcc8b5b6d60d917ed68c65093
adleywd <adleydamaceno@gmail.com>
2020-04-23 03:36:10 -0300

    Add likes function in api and web

--- DatingApp-SPA/src/app/services/user.service.ts
+++ DatingApp-SPA/src/app/services/user.service.ts
@@ -14,7 +14,7 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<User[]>> {
+  getUsers(page?, itemsPerPage?, userParams?, likesParam?): Observable<PaginatedResult<User[]>> {
     const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
     let params = new HttpParams();
 
@@ -30,16 +30,26 @@ export class UserService {
       params = params.append('orderBy', userParams.orderBy);
     }
 
-    return this.http.get<User[]>(this.baseUrl + 'users', {observe: 'response', params})
-    .pipe(
-      map(response => {
-        paginatedResult.result = response.body;
-        if (response.headers.get('Pagination') != null) {
-          paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'))
-        }
-        return paginatedResult;
-      })
-    );
+    if (likesParam === 'Likers') {
+      params = params.has('gender') ? params : params.append('gender', 'all');
+      params = params.append('likers', 'true');
+    }
+
+    if (likesParam === 'Likees') {
+      params = params.has('gender') ? params : params.append('gender', 'all');
+      params = params.append('likees', 'true');
+    }
+
+    return this.http.get<User[]>(this.baseUrl + 'users', { observe: 'response', params })
+      .pipe(
+        map(response => {
+          paginatedResult.result = response.body;
+          if (response.headers.get('Pagination') != null) {
+            paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'))
+          }
+          return paginatedResult;
+        })
+      );
   }
 
   getUser(id): Observable<User> {
@@ -55,7 +65,11 @@ export class UserService {
   }
 
   deletePhoto(userId: number, photoId: number) {
-  return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + photoId);
+    return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + photoId);
+  }
+
+  sendLike(id: number, recipientId: number) {
+    return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
   }
 
 }
