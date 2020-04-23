aa0c149075caaf61c15a69aff871fc21b4480611
adleywd <adleydamaceno@gmail.com>
2020-04-23 15:15:51 -0300

    Set up API to send and receive messages

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -14,18 +14,26 @@ namespace DatingApp.API.Helpers
                 .ForMember(dest => dest.PhotoUrl,
                     opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
                 .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()))
-
                 .ReverseMap();
+
             CreateMap<User, UserForDetailedDto>()
                 .ForMember(dest => dest.PhotoUrl,
                     opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
                 .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()))
                 .ReverseMap(); ;
+
             CreateMap<Photo, PhotoForDetailDto>().ReverseMap();
             CreateMap<UserForUpdateDto, User>().ReverseMap();
             CreateMap<Photo, PhotoForReturnDto>().ReverseMap();
             CreateMap<PhotoForCreationDto, Photo>().ReverseMap();
             CreateMap<UserForRegisterDto, User>().ReverseMap();
+            CreateMap<MessageForCreationDto, Message>().ReverseMap();
+            CreateMap<Message, MessageToReturnDto>()
+                .ForMember(dest => dest.SenderPhotoUrl, opt =>
+                    opt.MapFrom(u => u.Sender.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.RecipientPhotoUrl,
+                    opt => opt.MapFrom(u => u.Recipient.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ReverseMap();
         }
     }
 }
